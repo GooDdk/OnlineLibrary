fa4df3ba09c687407929a9393023298d30bb90ba
Kirill Šatilov <kirilly45@gmail.com>
2021-06-07 14:57:14 +0300

    Add files via upload

---
+++ book.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>OnlineLibrary</title>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;300;500;700&display=swap" rel="stylesheet">
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+	<nav class="topnav">
+  		<a href="./index.php" class="logo"><img src="img/logo.png" alt="logo" />   OnlineLibrary</a>
+  		<a href="./login.php" class="buttonnav">Log in</a>
+	</nav>
+
+	<div class="left-block">
+		<div class="bookcover">
+			<img alt="" src="">
+		</div>
+		<h1>Title</h1>
+		<h3 style="color: #728091;">Author</h3>
+
+		<div class="rating-mini">
+			<span class="active"></span>	
+			<span class="active"></span>    
+			<span class="active"></span>  
+			<span></span>    
+			<span></span>
+		</div>
+		<br>
+		<button name="submit" type="submit">Want to read</button>
+		<a href="./read.php" class="button" style="color: white; background-color: #728091;;">Read</a>
+	</div>
+
+	<div class="description">
+		<h1>Publisher description</h1>
+		<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, pariatur doloremque, ad rerum recusandae labore ut! Odio, architecto, expedita enim quam ullam ut facere aut rerum, doloribus magnam quisquam cum. Lorem ipsum dolor, sit amet consectetur adipisicing, elit. Recusandae sit ducimus sequi explicabo rerum praesentium qui numquam consequatur sed voluptas, dicta magnam velit laboriosam facere molestiae possimus vel iure, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora veniam officia eaque deserunt asperiores vel sint accusamus sequi pariatur unde aliquid quaerat nemo aliquam, velit. Atque nulla molestiae quaerat eum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eius nihil facilis quo. Neque mollitia, id expedita voluptate? Tenetur, nostrum ipsam facere a provident quia assumenda culpa. Consectetur explicabo, officia. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus porro, dolorem culpa exercitationem maxime delectus, harum magnam. Suscipit quidem voluptatum consectetur odio voluptatem. Distinctio libero, earum repudiandae, dolorum cum ipsa.</p>
+		<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, pariatur doloremque, ad rerum recusandae labore ut! Odio, architecto, expedita enim quam ullam ut facere aut rerum, doloribus magnam quisquam cum. Lorem ipsum dolor, sit amet consectetur adipisicing, elit. Recusandae sit ducimus sequi explicabo rerum praesentium qui numquam consequatur sed voluptas, dicta magnam velit laboriosam facere molestiae possimus vel iure, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora veniam officia eaque deserunt asperiores vel sint accusamus sequi pariatur unde aliquid quaerat nemo aliquam, velit. Atque nulla molestiae quaerat eum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores eius nihil facilis quo. Neque mollitia, id expedita voluptate? Tenetur, nostrum ipsam facere a provident quia assumenda culpa. Consectetur explicabo, officia. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus porro, dolorem culpa exercitationem maxime delectus, harum magnam. Suscipit quidem voluptatum consectetur odio voluptatem. Distinctio libero, earum repudiandae, dolorum cum ipsa.</p>
+	</div>
+
+</body>
+</html>
